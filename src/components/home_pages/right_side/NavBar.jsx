94f33cb28b8e595aa7fa994dbb60f2b6813b79f0
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-04-23 22:45:33 +0300

    Refactor the structure of the project.

--- src/components/home_pages/right_side/NavBar.jsx
+++ src/components/home_pages/right_side/NavBar.jsx
@@ -1,5 +1,3 @@
-import { GrGroup } from "react-icons/gr";
-
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useAuthContext } from "../../../context/AuthContext";
